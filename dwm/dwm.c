0bd6e8f1f9be4003cfd109e3c3312c9a7884d51b
wiszonic <example@example.com>
2026-02-15 21:55:17 +0100

    first status2d & slstatus rework

--- dwm/dwm.c
+++ dwm/dwm.c
@@ -168,6 +168,7 @@ static void detachstack(Client *c);
 static Monitor *dirtomon(int dir);
 static void drawbar(Monitor *m);
 static void drawbars(void);
+static void drawbevel(Drw *drw, int type, int style, int x, int y, int w, int h, int b, int scmtl, int scmbr);
 static int drawstatusbar(Monitor *m, int bh, char* text);
 static void enternotify(XEvent *e);
 static void expose(XEvent *e);
@@ -242,7 +243,7 @@ static void zoom(const Arg *arg);
 
 /* variables */
 static const char broken[] = "broken";
-static char stext[4096];
+static char stext[2048];
 static int screen;
 static int sw, sh;           /* X display screen geometry width, height */
 static int bh;               /* bar height */
@@ -725,115 +726,7 @@ dirtomon(int dir)
 	return m;
 }
 
-int
-drawstatusbar(Monitor *m, int bh, char* stext) {
-	int ret, i, w, x, len;
-	short isCode = 0;
-	char *text;
-	char *p;
-
-	len = strlen(stext) + 1 ;
-	if (!(text = (char*) malloc(sizeof(char)*len)))
-		die("malloc");
-	p = text;
-	memcpy(text, stext, len);
-
-	/* compute width of the status text */
-	w = 0;
-	i = -1;
-	while (text[++i]) {
-		if (text[i] == '^') {
-			if (!isCode) {
-				isCode = 1;
-				text[i] = '\0';
-				w += TEXTW(text) - lrpad;
-				text[i] = '^';
-				if (text[++i] == 'f')
-					w += atoi(text + ++i);
-			} else {
-				isCode = 0;
-				text = text + i + 1;
-				i = -1;
-			}
-		}
-	}
-	if (!isCode)
-		w += TEXTW(text) - lrpad;
-	else
-		isCode = 0;
-	text = p;
-
-	w += 2; /* 1px padding on both sides */
-	ret = x = m->ww - w;
-
-	drw_setscheme(drw, scheme[LENGTH(colors)]);
-	drw->scheme[ColFg] = scheme[SchemeNorm][ColFg];
-	drw->scheme[ColBg] = scheme[SchemeNorm][ColBg];
-	drw_rect(drw, x, 0, w, bh, 1, 1);
-	x++;
-
-	/* process status text */
-	i = -1;
-	while (text[++i]) {
-		if (text[i] == '^' && !isCode) {
-			isCode = 1;
-
-			text[i] = '\0';
-			w = TEXTW(text) - lrpad;
-			drw_text(drw, x, 0, w, bh, 0, text, 0);
-
-			x += w;
-
-			/* process code */
-			while (text[++i] != '^') {
-				if (text[i] == 'c') {
-					char buf[8];
-					memcpy(buf, (char*)text+i+1, 7);
-					buf[7] = '\0';
-					drw_clr_create(drw, &drw->scheme[ColFg], buf);
-					i += 7;
-				} else if (text[i] == 'b') {
-					char buf[8];
-					memcpy(buf, (char*)text+i+1, 7);
-					buf[7] = '\0';
-					drw_clr_create(drw, &drw->scheme[ColBg], buf);
-					i += 7;
-				} else if (text[i] == 'd') {
-					drw->scheme[ColFg] = scheme[SchemeNorm][ColFg];
-					drw->scheme[ColBg] = scheme[SchemeNorm][ColBg];
-				} else if (text[i] == 'r') {
-					int rx = atoi(text + ++i);
-					while (text[++i] != ',');
-					int ry = atoi(text + ++i);
-					while (text[++i] != ',');
-					int rw = atoi(text + ++i);
-					while (text[++i] != ',');
-					int rh = atoi(text + ++i);
-
-					drw_rect(drw, rx + x, ry, rw, rh, 1, 0);
-				} else if (text[i] == 'f') {
-					x += atoi(text + ++i);
-				}
-			}
-
-			text = text + i + 1;
-			i=-1;
-			isCode = 0;
-		}
-	}
-
-	if (!isCode) {
-		w = TEXTW(text) - lrpad;
-		drw_text(drw, x, 0, w, bh, 0, text, 0);
-	}
-
-	drw_setscheme(drw, scheme[SchemeNorm]);
-	free(p);
-
-	return ret;
-}
-
-static inline void
+static void
 drawbevel(Drw *drw, int type, int style,
 		int x, int y, int w, int h, int b,
 		int scmtl, int scmbr) 
@@ -861,6 +754,174 @@ drawbevel(Drw *drw, int type, int style,
 		drw_rect(drw, xi + b, yi + hi - b, wi, b, 1, 1);       	/* bottom */
 		drw_rect(drw, xi + wi, yi + b, b, hi - (b * 2), 1, 1); 	/* right */
 	}
+}
+
+int
+drawstatusbar(Monitor *m, int bh, char* stext) {
+	int ret, i, w, x, len,
+		y = tbpx, b = bevelpx,
+		og = outgappx, ig = ingappx,
+		tb = tbgappx, lr = lrgappx;
+
+	int xy = b * 3;
+	int h = bh - (y * 2);
+	int th = h - (xy * 2) - (tb * 2);
+	int ti = th + (tb * 2);
+	int yi = y + xy;
+	int yh = yi + tb;
+	int wi = (xy * 2) + (lr * 2) + (b * 3);
+	int wh = xy + lr;
+
+	short isCode = 0;
+	char *text;
+	char *p;
+
+	len = strlen(stext) + 1 ;
+	if (!(text = (char*) malloc(sizeof(char)*len)))
+		die("malloc");
+	p = text;
+	memcpy(text, stext, len);
+
+	/* compute width of the status text */
+	w = 0;
+	i = -1;
+	while (text[++i]) {
+		if (text[i] == '^') {
+			if (!isCode) {
+				isCode = 1;
+				text[i] = '\0';
+				w += TEXTW(text) - lrpad;
+				text[i] = '^';
+				if (text[++i] == 'f') {
+					w += atoi(text + ++i);
+				} else if (text[i] == 'o') {
+					i++;
+					if (text[i] == '1') {
+						w += wi + og + b;
+					} else if (text[i] == '2') {
+						w += wi;
+					}
+				} else if (text[i] == 'i') {
+					i++;
+					if (text[i] == '1') {
+						w += 0;
+					} else if (text[i] == '2') {
+						w += (b * 3) + ig + b;
+					} else if (text[i] == '3') {
+						w += 0;
+					}
+				} else if (text[i] == 's') {
+					w += b * 2;
+				}
+			} else {
+				isCode = 0;
+				text = text + i + 1;
+				i = -1;
+			}
+		}
+	}
+	if (!isCode)
+		w += TEXTW(text) - lrpad;
+	else
+		isCode = 0;
+	text = p;
+
+	w += og + lrpx + b; /* adjustable padding on both sides */
+	ret = x = m->ww - w;
+
+	drw_setscheme(drw, scheme[SchemeItem]);
+	drw_rect(drw, x + og + xy, yi, w - og - xy - lrpx, ti, 1, 1);
+	x += og;
+
+	/* process status text */
+	i = -1;
+	int ox = x;
+	int ix = ox;
+	while (text[++i]) {
+		if (text[i] == '^' && !isCode) {
+			isCode = 1;
+
+			text[i] = '\0';
+			w = TEXTW(text) - lrpad;
+			drw_text(drw, x + wh + (b * 2), yh, w, th, 0, text, 0);
+
+			x += w;
+
+			/* process code */
+			while (text[++i] != '^') {
+				if (text[i] == 'f') {
+					x += atoi(text + ++i);
+				} else if (text[i] == 'o') {
+					i++;
+					int ow = x - ox + wi;
+					if (text[i] == '1') {
+						drawbevel(drw, 3, 0, ox, y, ow, h, b,
+							SchemeBR1, SchemeTL1);
+						drw_setscheme(drw, scheme[SchemeNorm]);
+						drw_rect(drw, ox + ow + b, yi, og, ti, 1, 1);
+						x += wi + og + b;
+					} else if (text[i] == '2') {
+						drawbevel(drw, 3, 1, ox, y, ow, h, b,
+							SchemeTL1, SchemeBR1);					
+					}
+					ox = x;
+					ix = ox;
+				} else if (text[i] == 'i') {
+					i++;
+					int iw = x - ix + (b * 3);
+					if (text[i] == '1') {
+						drawbevel(drw, 2, 0, ix + wh, yh, iw, th, b,
+							SchemeTL1, SchemeBR1);					
+					} else if (text[i] == '2') {
+						drawbevel(drw, 2, 0, ix + wh, yh, iw, th, b,
+							SchemeBR2, SchemeTL2);					
+						drw_setscheme(drw, scheme[SchemeSel]);
+						drw_rect(drw, ix + wh + (b * 2), yh + (b * 2),
+							lr + (b * 2), th - (b * 4), 1, 1);
+						drw_rect(drw, ix + iw - lr + (b * 4), yh + (b * 2),
+							lr + (b * 2), th - (b * 4), 1, 1);
+						x += (b * 3) + ig + b;
+					} else if (text[i] == '3') {
+						drawbevel(drw, 2, 0, ix + wh, yh, iw, th, b,
+							SchemeTL2, SchemeBR2);					
+						drw_setscheme(drw, scheme[SchemeSel]);
+						drw_rect(drw, ix + wh + (b * 2), yh + (b * 2),
+							lr + (b * 2), th - (b * 4), 1, 1);
+						drw_rect(drw, ix + iw - lr + (b * 4), yh + (b * 2),
+							lr + (b * 2), th - (b * 4), 1, 1);
+					}
+					ix = x;
+				} else if (text[i] == 's') {
+					drw_setscheme(drw, scheme[SchemeBR1]);
+					drw_rect(drw, x + wh + (b * 2), yh, b, th, 1, 1);
+					drw_setscheme(drw, scheme[SchemeTL1]);
+					drw_rect(drw, x + wh + xy, yh, b, th, 1, 1);
+					x += b * 2;
+				} else if (text[i] == 'b') {
+					i++;
+					if (text[i] == '1') {
+						drw_setscheme(drw, scheme[SchemeItem]);
+					} else if (text[i] == '2') {
+						drw_setscheme(drw, scheme[SchemeSel]);
+					}
+				}
+			}
+
+			text = text + i + 1;
+			i = -1;
+			isCode = 0;
+		}
+	}
+
+	if (!isCode) {
+		w = TEXTW(text) - lrpad;
+		drw_text(drw, x, yi, w, ti, 0, text, 0);
+	}
+
+	drw_setscheme(drw, scheme[SchemeNorm]);
+	free(p);
+
+	return ret;
 }
 
 void
--- slstatus/config.h
+++ slstatus/config.h
@@ -7,7 +7,35 @@ const unsigned int interval = 1000;
 static const char unknown_str[] = "U";
 
 /* maximum command output length */
-#define CMDLEN 4096
+#define CMDLEN 2048
+
+/* define commands */
+static const char keycmd[] 	= "setxkbmap -print | awk -F'+' '/xkb_symbols/ {print $2}' | tr 'a-z' 'A-Z'";
+static const char volcmd[] 	= "wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print int($2 * 100)}'";
+static const char volmutcmd[] 	= "wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print substr($3, 2, 1)}'";
+static const char miccmd[]	= "wpctl get-volume @DEFAULT_AUDIO_SOURCE@ | awk '{print int($2 * 100)}'";
+static const char micmutcmd[] 	= "wpctl get-volume @DEFAULT_AUDIO_SOURCE@ | awk '{print substr($3, 2, 1)}'";
+static const char moncmd[]	= "xrandr --verbose | grep -i brightness | awk '{print $2}'";
+
+/* define output */
+static const struct arg args[] 	= {
+	/* function 	format          		argument 	turn	signal */
+	{ run_command, 	"^b1^^f6^%s^f6^^s^",           	"echo 'Key'", 	0,   	-1 },
+	{ run_command, 	"^b1^^f6^%s^f6^^i1^^o1^",       keycmd, 	1,   	-1 },
+	{ run_command, 	"^b1^^f6^%s^f6^^s^",           	"echo 'Vol'", 	0,   	-1 },
+	{ run_command, 	"^b1^^f6^%s%%^f6^^s^",         	volcmd, 	1,   	-1 },
+	{ run_command, 	"^b1^^f6^%s^f6^^i1^^o1^",       volmutcmd, 	1,   	-1 },
+	{ run_command, 	"^b1^^f6^%s^f6^^s^",           	"echo 'Mic'", 	0,   	-1 },
+	{ run_command, 	"^b1^^f6^%s%%^f6^^s^",         	miccmd, 	1,   	-1 },
+	{ run_command, 	"^b1^^f6^%s^f6^^i1^^o1^",       micmutcmd, 	1,   	-1 },
+	{ run_command, 	"^b1^^f6^%s^f6^^s^",        	"echo 'Bri'", 	0,   	-1 },
+	{ run_command, 	"^b1^^f6^%s^f6^^i1^^o1^",       moncmd, 	1,   	-1 },
+	{ datetime,	"^b2^^f6^%s^f6^^i2^",		"%H:%M", 	1, 	-1 },
+	{ datetime,	"^b2^^f6^%s^f6^^i3^^o2^",	"%a %d. %b", 	1, 	-1 },
+};
+
+/* maximum output string length */
+#define MAXLEN CMDLEN * LEN(args)
 
 /*
  * function            description                     argument (example)
@@ -65,131 +93,3 @@ static const char unknown_str[] = "U";
  * wifi_perc           WiFi signal in percent          interface name (wlan0)
  */
 
-/* define commands */
-static const char keycmd[] 	= "setxkbmap -print | awk -F'+' '/xkb_symbols/ {print $2}' | tr 'a-z' 'A-Z'";
-static const char volcmd[] 	= "wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print int($2 * 100)}'";
-static const char volmutcmd[] 	= "wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print substr($3, 2, 1)}'";
-static const char miccmd[]	= "wpctl get-volume @DEFAULT_AUDIO_SOURCE@ | awk '{print int($2 * 100)}'";
-static const char micmutcmd[] 	= "wpctl get-volume @DEFAULT_AUDIO_SOURCE@ | awk '{print substr($3, 2, 1)}'";
-
-static const struct arg args[] = {
-
-        /* key */
-	{ run_command,
-		"^c#3a3150^^r0,4,45,1^^r0,5,1,34^^f1^^r0,5,44,1^^r0,6,44,1^^r0,7,1,31^^r1,7,1,30 \
-		^^c#8c5dd9^^r2,36,42,1^^r1,37,43,1^^r0,38,44,1 \
-		^^f2^^c#4b3f66^^r0,7,42,29 \
-		^^f4^^c#8c5dd9^^r0,11,38,1^^r0,12,38,1^^r0,13,1,19^^r1,13,1,18 \
-		^^c#3a3150^^r2,30,36,1^^r1,31,37,1 \
-		^^f8^^c#eeeeee^^b#4b3f66^%s^f6 \
-		^^c#3a3150^^r0,13,1,17 \
-		^^c#8c5dd9^^r1,13,1,17^",
-		"echo 'Key'", 0, -1 },
-        { run_command,  
-		"^f2^^c#3a3150^^r0,4,37,1^^r0,5,36,1^^r0,6,35,1 \
-		^^c#8c5dd9^^r0,36,36,1^^r0,37,36,1^^r0,38,37,1 \
-		^^c#4b3f66^^r0,7,34,29 \
-		^^c#8c5dd9^^r0,11,30,1^^r0,12,29,1 \
-		^^c#3a3150^^r0,30,30,1^^r0,31,30,1 \
-		^^f6^^c#eeeeee^^b#4b3f66^%s^f6 \
-		^^c#3a3150^^r0,13,1,17^^r1,12,1,18^^f6 \
-		^^c#8c5dd9^^r0,7,1,29^^r1,6,1,30^^r2,5,1,33^^f7^",                                 
-		keycmd, 5, -1 },
-
-        /* vol */
-        { run_command,
-		"^c#3a3150^^r0,4,44,1^^r0,5,1,34^^f1^^r0,5,43,1^^r0,6,43,1^^r0,7,1,31^^r1,7,1,30 \
-		^^c#8c5dd9^^r2,36,41,1^^r1,37,42,1^^r0,38,43,1 \
-		^^f2^^c#4b3f66^^r0,7,41,29 \
-		^^f4^^c#8c5dd9^^r0,11,37,1^^r0,12,37,1^^r0,13,1,19^^r1,13,1,18 \
-		^^c#3a3150^^r2,30,35,1^^r1,31,36,1 \
-		^^f8^^c#eeeeee^^b#4b3f66^%s^f6 \
-		^^c#3a3150^^r0,13,1,17 \
-		^^c#8c5dd9^^r1,13,1,17^",
-		"echo 'Vol'", 0, -1 },
-        { run_command,
-		"^f2^^c#3a3150^^r0,4,41,1^^r0,5,41,1^^r0,6,41,1 \
-		^^c#8c5dd9^^r0,36,41,1^^r0,37,41,1^^r0,38,41,1 \
-		^^c#4b3f66^^r0,7,41,29 \
-		^^c#8c5dd9^^r0,11,41,1^^r0,12,41,1 \
-		^^c#3a3150^^r0,30,41,1^^r0,31,41,1 \
-		^^f6^^c#eeeeee^^b#4b3f66^%s%%^f6 \
-		^^c#3a3150^^r0,13,1,17 \
-		^^c#8c5dd9^^r1,13,1,17^",
-		volcmd, 5, -1 },
-	{ run_command,  
-		"^f2^^c#3a3150^^r0,4,30,1^^r0,5,29,1^^r0,6,28,1 \
-		^^c#8c5dd9^^r0,36,29,1^^r0,37,29,1^^r0,38,30,1 \
-		^^c#4b3f66^^r0,7,27,29 \
-		^^c#8c5dd9^^r0,11,23,1^^r0,12,22,1 \
-		^^c#3a3150^^r0,30,23,1^^r0,31,23,1 \
-		^^f6^^c#eeeeee^^b#4b3f66^%s^f6 \
-		^^c#3a3150^^r0,13,1,17^^r1,12,1,18^^f6 \
-		^^c#8c5dd9^^r0,7,1,29^^r1,6,1,30^^r2,5,1,33^^f7^",
-		volmutcmd, 5, -1 },
-
-        /* mic */
-        { run_command,
-		"^c#3a3150^^r0,4,44,1^^r0,5,1,34^^f1^^r0,5,43,1^^r0,6,43,1^^r0,7,1,31^^r1,7,1,30 \
-		^^c#8c5dd9^^r2,36,41,1^^r1,37,42,1^^r0,38,43,1 \
-		^^f2^^c#4b3f66^^r0,7,41,29 \
-		^^f4^^c#8c5dd9^^r0,11,37,1^^r0,12,37,1^^r0,13,1,19^^r1,13,1,18 \
-		^^c#3a3150^^r2,30,35,1^^r1,31,36,1 \
-		^^f8^^c#eeeeee^^b#4b3f66^%s^f6 \
-		^^c#3a3150^^r0,13,1,17 \
-		^^c#8c5dd9^^r1,13,1,17^",
-		"echo 'Mic'", 0, -1 },
-        { run_command,
-		"^f2^^c#3a3150^^r0,4,41,1^^r0,5,41,1^^r0,6,41,1 \
-		^^c#8c5dd9^^r0,36,41,1^^r0,37,41,1^^r0,38,41,1 \
-		^^c#4b3f66^^r0,7,41,29 \
-		^^c#8c5dd9^^r0,11,41,1^^r0,12,41,1 \
-		^^c#3a3150^^r0,30,41,1^^r0,31,41,1 \
-		^^f6^^c#eeeeee^^b#4b3f66^%s%%^f6 \
-		^^c#3a3150^^r0,13,1,17 \
-		^^c#8c5dd9^^r1,13,1,17^",
-		miccmd, 5, -1 },
-	{ run_command,
-		"^f2^^c#3a3150^^r0,4,30,1^^r0,5,29,1^^r0,6,28,1 \
-		^^c#8c5dd9^^r0,36,29,1^^r0,37,29,1^^r0,38,30,1 \
-		^^c#4b3f66^^r0,7,27,29 \
-		^^c#8c5dd9^^r0,11,23,1^^r0,12,22,1 \
-		^^c#3a3150^^r0,30,23,1^^r0,31,23,1 \
-		^^f6^^c#eeeeee^^b#4b3f66^%s^f6 \
-		^^c#3a3150^^r0,13,1,17^^r1,12,1,18^^f6 \
-		^^c#8c5dd9^^r0,7,1,29^^r1,6,1,30^^r2,5,1,33^^f7^",
-		micmutcmd, 5, -1 },
-
-        /* time and date */
-        { datetime,
-		"^c#3a3150^^r0,4,58,1^^r0,5,1,34^^f1 \
-		^^c#8c5dd9^^r0,5,57,1^^r0,6,57,1^^r0,7,1,31^^r1,7,1,30 \
-		^^c#3a3150^^r2,36,55,1^^r1,37,56,1 \
-		^^c#8c5dd9^^r0,38,57,1 \
-		^^f2^^c#4b3f66^^r0,7,55,29 \
-		^^c#a86f46^^r6,13,6,17 \
-		^^f4^^c#875534^^r0,11,51,1^^r0,12,50,1^^r0,13,1,19^^r1,13,1,18 \
-		^^c#f6c175^^r2,30,49,1^^r1,31,50,1 \
-		^^f8^^c#eeeeee^^b#a86f46^%s^f6 \
-		^^c#a86f46^^r-6,13,6,17 \
-		^^c#f6c175^^r0,13,1,17^^r1,12,1,18^",
-		"%H:%M", 1, -1 },
-	{ datetime,
-		"^f2^^c#3a3150^^r0,4,95,1 \
-		^^c#8c5dd9^^r0,5,94,1^^r0,6,93,1 \
-		^^c#3a3150^^r0,36,94,1^^r0,37,94,1 \
-		^^c#8c5dd9^^r0,38,95,1 \
-		^^c#4b3f66^^r0,7,92,29 \
-		^^f2^^c#a86f46^^r2,13,6,17 \
-		^^c#f6c175^^r0,11,86,1^^r0,12,85,1^^r0,13,1,19^^r1,13,1,18 \
-		^^c#875534^^r2,30,84,1^^r1,31,85,1 \
-		^^f8^^c#eeeeee^^b#a86f46^%s^f6 \
-		^^c#a86f46^^r-6,13,6,17 \
-		^^c#875534^^r0,13,1,17^^r1,12,1,18^^f6 \
-		^^c#3a3150^^r0,7,1,29^^r1,6,1,30 \
-		^^c#8c5dd9^^r2,5,1,33^^f3^",
-		"%a %d. %b", 1, -1 },
-};
-
-/* maximum output string length */
-#define MAXLEN CMDLEN * LEN(args)
